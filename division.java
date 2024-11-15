c91c6e770e9be4531630bac758a1042f03488895
manijayavel <jkmaniromiyo@gmail.com>
2024-11-15 17:12:43 +0530

    Add files via upload

---
+++ division.java
@@ -0,0 +1,19 @@
+package javaprogramming;
+
+import java.util.Scanner;
+
+public class division {
+	
+	public static void main(String[] args) {
+		Scanner obj=new Scanner(System.in);
+		System.out.println("Enter A value=");
+		int a=obj.nextInt();
+		System.out.println("Enter b value=");
+		int b=obj.nextInt();
+		int c=(a/b);
+		System.out.println(c);
+		
+
+	}
+
+}
