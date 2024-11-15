db557f18bfc33e78c95f54418a923a652b799776
manijayavel <jkmaniromiyo@gmail.com>
2024-11-15 17:13:59 +0530

    Add files via upload

---
+++ AssingnmentOperator.java
@@ -0,0 +1,33 @@
+package javaprogramming;
+
+public class AssingnmentOperator {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int num=10;
+		String name="java";
+		System.out.println("num"+num);
+		System.out.println("name"+name);
+		//+=operator
+		int a=10;
+		a+=10;//a=a+10
+		
+		System.out.println("value of a"+a);
+		
+		//&&(and)operator>-grater
+		System.out.println((5>3) && (8>5));
+		
+		//&&(and)operator<-lesser
+		System.out.println((5<3) && (8<7));
+		
+		// ||(and)operator >-greater
+		System.out.println("||-or operator");
+		System.out.println((5<3) || (8<5));
+		
+		//! not operator
+		System.out.println("! -Not operator");
+		System.out.println(!(5<3));
+		
+		}
+
+}
