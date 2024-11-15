bacecc7dbc35bfb35654261c2966d378d8ee7039
manijayavel <jkmaniromiyo@gmail.com>
2024-11-15 17:13:12 +0530

    Add files via upload

---
+++ datatype.java
@@ -0,0 +1,18 @@
+package javaprogramming;
+
+public class datatype {
+
+	public static void main(String[] args) {
+		int a=10;
+		int b=20;
+		int c=30;
+		char var1='a';
+		char var2='b';
+		double var3=45.45;
+		
+		
+	System.out.println(a=b);
+
+	}
+
+}
