996da3ab6da13ca6f0cb2b76155c13090c40e7d5
manijayavel <jkmaniromiyo@gmail.com>
2024-11-15 17:14:31 +0530

    Add files via upload

---
+++ relationallogicaloperatortask3.java
@@ -0,0 +1,28 @@
+package javaprogramming;
+
+public class relationallogicaloperatortask3{
+	public static void main(String[]args) {
+		int a=10;
+		int b=20;
+		
+		boolean x=true;
+		boolean y=true;
+		
+		
+		//relational operator
+		System.out.println(a<b);
+		System.out.println(a>b);
+		System.out.println(a<=b);
+		System.out.println(a>=b);
+		System.out.println(a==b);
+		System.out.println(a!=b);
+		//logical operator
+		System.out.println(x&&y);
+		System.out.println(x||y);
+		System.out.println(!x);
+		System.out.println(!y);
+		
+		
+	}
+
+}
