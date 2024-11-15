0f2b2571628fc13b966094e32066d070197ed0b1
manijayavel <jkmaniromiyo@gmail.com>
2024-11-15 17:17:24 +0530

    Add files via upload

---
+++ methodparametertask1.java
@@ -0,0 +1,17 @@
+package javaprogramming;
+
+public class methodparametertask1{
+	public static void adde(int num1,int num2) {
+		int sum=num1+num2;
+		System.out.println(sum);
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int num1=10;
+		int num2=20;
+		adde( num1, num2);
+
+	}
+
+}
