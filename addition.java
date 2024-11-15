dcd761a2b32dd8a5ff2448120139167a0b2f641d
manijayavel <jkmaniromiyo@gmail.com>
2024-11-15 17:11:12 +0530

    Add files via upload

---
+++ addition.java
@@ -0,0 +1,16 @@
+package javaprogramming;
+import java.util.*;
+public class addition {
+
+	public static void main(String[] args) {
+		Scanner obj=new Scanner(System.in);
+		System.out.println("Enter A value=");
+		int a= obj.nextInt();
+		System.out.println("Enter B value=");
+		int b= obj.nextInt();
+		int c=(a+b);
+		System.out.println("the addition of c value is "+c );
+
+	}
+
+}
