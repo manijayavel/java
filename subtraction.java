0a64772f3b4f60e3bd0321621ed57b111aa6274a
manijayavel <jkmaniromiyo@gmail.com>
2024-11-15 17:11:48 +0530

    Add files via upload

---
+++ subtraction.java
@@ -0,0 +1,18 @@
+package javaprogramming;
+
+import java.util.Scanner;
+
+public class subtraction {
+
+	public static void main(String[] args) {
+		Scanner obj=new Scanner(System.in);
+		System.out.println("Enter A value=");
+		int a=obj.nextInt();
+		System.out.println("Enter b value=");
+		int b=obj.nextInt();
+		int c=(a-b);
+		System.out.println("the subtraction of c value is "+c);
+
+	}
+
+}
