c1424d1d5f059468aa35794900def6885e71148a
manijayavel <jkmaniromiyo@gmail.com>
2024-11-19 09:58:14 +0530

    Add files via upload

---
+++ whiletask1.java
@@ -0,0 +1,15 @@
+package javaprogramming;
+
+public class whiletask1{
+	public static void mani(String[]args)
+	{
+		int i=10;
+		while(i<100)
+		{
+			System.out.println(i+" ");
+			i=i+10;
+			
+		}
+	}
+
+}
