e310dcdda95cf328f11903539847a0a454d7efee
manijayavel <jkmaniromiyo@gmail.com>
2024-11-19 09:58:40 +0530

    Add files via upload

---
+++ evenoddtask.java
@@ -0,0 +1,21 @@
+package javaprogramming;
+import java.util.*;
+public class evenoddtask {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner obj=new Scanner(System.in);
+		int a=obj.nextInt();
+		if(a%2==0)
+		{
+			System.out.println("even number"+a);
+		}
+		else {
+			System.out.println("ood number"+a);
+		}
+		
+
+	}
+
+}
+
