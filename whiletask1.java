da7a7bdcd1348295030dd1a5e55957eb70d67bda
manijayavel <jkmaniromiyo@gmail.com>
2024-11-19 16:58:21 +0530

    Update whiletask1.java

--- whiletask1.java
+++ whiletask1.java
@@ -1,10 +1,10 @@
 package javaprogramming;
 
 public class whiletask1{
-	public static void mani(String[]args)
+	public static void main(String[]args)
 	{
 		int i=10;
-		while(i<100)
+		while(i<=100)
 		{
 			System.out.println(i+" ");
 			i=i+10;
