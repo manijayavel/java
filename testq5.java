f1421ca613f6977e7124e11a75f31b8fc8537c30
manijayavel <jkmaniromiyo@gmail.com>
2024-11-29 09:36:17 +0530

    Add files via upload

---
+++ testq5.java
@@ -0,0 +1,36 @@
+package javaprogramming;
+
+public class testq5 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int num=500;
+		System.out.println(num);
+		if(num>=400)
+		{
+			num-=100;
+			System.out.println(num);
+			if(num>=300)
+			{
+				num-=100;
+				System.out.println(num);
+				if(num>=200)
+				{
+					num-=100;
+				System.out.println(num);
+				if(num>=100)
+				{
+					num-=100;
+					System.out.println(num);
+					if(num>=100)
+						num-=100;
+					System.out.println(num);
+				}
+				}
+			}
+			
+		}
+
+	}
+
+}
