6fe97819ff33bdd37570c95e06f1759e5fdaae71
manijayavel <jkmaniromiyo@gmail.com>
2024-12-13 11:58:43 +0530

    Add files via upload

---
+++ pattentask13/Righttriangle.java
@@ -0,0 +1,16 @@
+package task;
+
+public class Righttriangle {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int i,j,star=5;
+		for(i=1;i<=star;i++) {
+			for(j=1;j<=i;j++) {
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+	}
+
+}
